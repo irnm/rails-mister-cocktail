d6d74e2dbe4b603a04fc3c9cfd4f8b79d8ae3f87
Irene Fernandes <fernandesm.irene@gmail.com>
2019-08-22 19:26:52 +0100

    solution mister cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+require "json"
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating ingredients...'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)
+
+ingredients['drinks'].each do |drink|
+  Ingredient.create(name: drink["strIngredient1"])
+end
